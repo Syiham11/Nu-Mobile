6166c55ff5569218d15256469b0e575d5550fa28
SitiSifa <siti.sitisifa@gmail.com>
2016-12-18 18:23:30 +0700

    modifikasi surat keluar

--- app/src/main/java/com/adfin/numobile/activity/kearsipan/KearsipanSuratKeluar.java
+++ app/src/main/java/com/adfin/numobile/activity/kearsipan/KearsipanSuratKeluar.java
@@ -1,9 +1,14 @@
 package com.adfin.numobile.activity.kearsipan;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.adfin.numobile.R;
+import com.adfin.numobile.activity.kegiatan.KegiatanMenuActivity;
+import com.adfin.numobile.activity.kegiatan.PeristiwaBoardcast;
 
 public class KearsipanSuratKeluar extends AppCompatActivity {
 
@@ -11,5 +16,14 @@ public class KearsipanSuratKeluar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kearsipan_surat_keluar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(KearsipanSuratKeluar.this, KearsipanMenuActivity.class);
+                startActivityForResult(i, 1);
+            }
+        });
     }
 }
