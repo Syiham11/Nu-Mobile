38bafa57d0171ddd2c7751360eead04ef3e7358c
Nedya Amrih Praksa <nedya@detik.com>
2016-12-19 03:38:03 +0700

    Update Peristiwa Foto

--- app/src/main/java/com/adfin/numobile/helper/GPSTracker.java
+++ app/src/main/java/com/adfin/numobile/helper/GPSTracker.java
@@ -1,21 +1,21 @@
 package com.adfin.numobile.helper;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
-import android.util.Log;
+import android.support.v4.app.ActivityCompat;
 
-/**
- * Created by prakasa on 17/12/16.
- */
+// Created by prakasa on 17/12/16.
 
 public class GPSTracker extends Service implements LocationListener {
     private final Context mContext;
@@ -37,7 +37,7 @@ public class GPSTracker extends Service implements LocationListener {
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 1 meters
 
     // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 500 * 60 * 1; // 1 minute
+    private static final long MIN_TIME_BW_UPDATES = 500 * 60; // 1 minute
 
     // Declaring a Location Manager
     protected LocationManager locationManager;
@@ -170,8 +170,6 @@ public class GPSTracker extends Service implements LocationListener {
                 dialog.cancel();
             }
         });
-
-        // Showing Alert Message
         alertDialog.show();
     }
 
--- app/src/main/java/com/adfin/numobile/helper/ListAdapterAll.java
+++ app/src/main/java/com/adfin/numobile/helper/ListAdapterAll.java
@@ -2,7 +2,6 @@ package com.adfin.numobile.helper;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,26 +10,21 @@ import android.widget.Toast;
 import com.adfin.numobile.R;
 import com.squareup.picasso.Picasso;
 
-/**
- * Created by Siti on 12/4/2016.
- */
+// Created by Siti on 12/4/2016.
 
 public class ListAdapterAll extends  RecyclerView.Adapter<ViewHolderAll>{
 
-//deklarasi variable context
-
     private final Context context;
 
-    String [] imag={};
-    String [] name={};
-    String [] lain={};
-    // menampilkan list item dalam bentuk text dengan tipe data string dengan variable name
+    private String [] imag={};
+    private String [] name={};
+    private String [] lain={};
 
-    LayoutInflater inflater;
+    private LayoutInflater inflater;
 
     public ListAdapterAll(Context context, String[] imag, String[] name, String[] lain) {
+        inflater = LayoutInflater.from(context);
         this.context=context;
-        inflater=LayoutInflater.from(context);
         this.imag = imag;
         this.name = name;
         this.lain = lain;
@@ -39,8 +33,7 @@ public class ListAdapterAll extends  RecyclerView.Adapter<ViewHolderAll>{
     public ViewHolderAll onCreateViewHolder(ViewGroup parent, int viewType) {
         View v=inflater.inflate(R.layout.list_content, parent, false);
 
-        ViewHolderAll viewHolder=new ViewHolderAll(v);
-        return viewHolder;
+        return new ViewHolderAll(v);
     }
 
     @Override
@@ -57,22 +50,24 @@ public class ListAdapterAll extends  RecyclerView.Adapter<ViewHolderAll>{
                     .load(this.imag[position])
                     .resize(70, 70)
                     .into(holder.imageView);
+        }else{
+            int[] imgArr = new int[] {R.drawable.numobile};
+
+            Picasso.with(this.context)
+                    .load(imgArr[0])
+                    .placeholder(R.drawable.numobile)
+                    .into(holder.imageView);
         }
     }
 
-    View.OnClickListener clickListener=new View.OnClickListener() {
+    private View.OnClickListener clickListener=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            //member aksi saat cardview diklik berdasarkan posisi tertentu
             ViewHolderAll vholder = (ViewHolderAll) v.getTag();
-
-            int position = vholder.getPosition();
-
+            int position = vholder.getAdapterPosition();
             Toast.makeText(context, "Menu ini berada di posisi " + position, Toast.LENGTH_LONG).show();
         }
     };
-
-
 
     @Override
     public int getItemCount() {
