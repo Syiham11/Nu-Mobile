6d2dfa5fdbeb8a55337c56653322bfe8a47360be
SitiSifa <siti.sitisifa@gmail.com>
2016-12-18 18:24:03 +0700

    modifikasi surat masuk

--- app/src/main/java/com/adfin/numobile/activity/kearsipan/KearsipanSuratMasuk.java
+++ app/src/main/java/com/adfin/numobile/activity/kearsipan/KearsipanSuratMasuk.java
@@ -1,15 +1,311 @@
 package com.adfin.numobile.activity.kearsipan;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.adfin.numobile.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+
 public class KearsipanSuratMasuk extends AppCompatActivity {
+
+    EditText dari, kepada, subject;
+    ImageView imageView;
+
+    String pathImage = "";
+    final Context context = this;
+
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    private static final int CAMERA_PICK_IMAGE_REQUEST_CODE = 200;
+    private static final String IMAGE_DIRECTORY_NAME = "NU";
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_PICK = 2;
+    private Uri fileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kearsipan_surat_masuk);
+
+        dari = (EditText) findViewById(R.id.editDari);
+        kepada = (EditText) findViewById(R.id.editKepada);
+        subject = (EditText) findViewById(R.id.editSubjek);
+
+        Button buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+    }
+
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+    private void browseImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/jpg");
+        startActivityForResult(
+                Intent.createChooser(intent, "Select File"),
+                CAMERA_PICK_IMAGE_REQUEST_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                previewCapturedImage();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }else if (requestCode == CAMERA_PICK_IMAGE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImageUri = data.getData();
+                String[] projection = { MediaStore.MediaColumns.DATA };
+                CursorLoader cursorLoader = new CursorLoader(this,selectedImageUri, projection, null, null,
+                        null);
+                Cursor cursor =cursorLoader.loadInBackground();
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+                cursor.moveToFirst();
+                String selectedImagePath = cursor.getString(column_index);
+                pathImage = cursor.getString(column_index);
+                Bitmap bm;
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(selectedImagePath, options);
+                final int REQUIRED_SIZE = 200;
+                int scale = 1;
+                while (options.outWidth / scale / 2 >= REQUIRED_SIZE
+                        && options.outHeight / scale / 2 >= REQUIRED_SIZE)
+                    scale *= 2;
+                options.inSampleSize = scale;
+                options.inJustDecodeBounds = false;
+                bm = BitmapFactory.decodeFile(selectedImagePath, options);
+                imageView.setImageBitmap(bm);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image picker", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to pick image", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    private void previewCapturedImage() {
+        try {
+            imageView.setVisibility(View.VISIBLE);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
+                    options);
+            pathImage = fileUri.getPath();
+
+            imageView.setImageBitmap(bitmap);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private static File getOutputMediaFile(int type) {
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+
+//    protected synchronized void buildGoogleApiClient() {
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(LocationServices.API)
+//                .build();
+//    }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        mGoogleApiClient.connect();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        if (mGoogleApiClient.isConnected()) {
+//            mGoogleApiClient.disconnect();
+//        }
+//    }
+
+//    /**
+//     * Runs when a GoogleApiClient object successfully connects.
+//     */
+//    @Override
+//    public void onConnected(Bundle connectionHint) {
+//        // Provides a simple way of getting a device's location and is well suited for
+//        // applications that do not require a fine-grained location and that do not need location
+//        // updates. Gets the best and most recent location currently available, which may be null
+//        // in rare cases when a location is not available.
+//        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//        if (mLastLocation != null) {
+//            mLatitudeLabel = String.format("%f", mLastLocation.getLatitude());
+//            mLongitudeLabel = String.format("%f", mLastLocation.getLongitude());
+//
+//            textLocation.setText("Lokasi, " + mLatitudeLabel + " & " + mLongitudeLabel);
+//        } else {
+//            textLocation.setText("Lokasi tidak ditemukan.");
+//        }
+//    }
+//
+//    @Override
+//    public void onConnectionFailed(ConnectionResult result) {
+//        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+//        // onConnectionFailed.
+////        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+//    }
+//
+//
+//    @Override
+//    public void onConnectionSuspended(int cause) {
+//        // The connection to Google Play services was lost for some reason. We call connect() to
+//        // attempt to re-establish the connection.
+////        Log.i(TAG, "Connection suspended");
+//        mGoogleApiClient.connect();
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_new) {
+            // custom dialog
+            final Dialog dialog = new Dialog(context);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.dialog_photo);
+            dialog.setTitle("");
+
+            Button browseButton = (Button) dialog.findViewById(R.id.buttonBrowse);
+
+            browseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    browseImage();
+                    dialog.dismiss();
+                }
+            });
+
+            Button captureButton = (Button) dialog.findViewById(R.id.buttonCapture);
+
+            captureButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    captureImage();
+                    dialog.dismiss();
+                }
+            });
+
+            dialog.show();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void saveImage(Bitmap bitmap) {
+        File file = new File(fileUri.getPath());
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/KKN");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-" + n + ".jpg";
+        file = new File(myDir, fname);
+
+        if (file.exists())
+            file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent returnIntent = new Intent();
+        setResult(2, returnIntent);
+        finish();
     }
 }
