5672bc78e5ae6d312768893643873ebe49107014
SitiSifa <siti.sitisifa@gmail.com>
2017-01-14 17:47:13 +0700

    add model lish data member user

---
+++ app/src/main/java/com/adfin/numobile/model/CDataMemberUser.java
@@ -0,0 +1,21 @@
+package com.adfin.numobile.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Siti on 1/14/2017.
+ */
+
+public class CDataMemberUser {
+
+    private List<DataMemberUser> DataMemberUser = new ArrayList<DataMemberUser>();
+
+    public List<DataMemberUser> getDataMemberUser() {
+        return DataMemberUser;
+    }
+
+    public void setDataMemberUser(List<DataMemberUser> DataMemberUser) {
+        this.DataMemberUser = DataMemberUser;
+    }
+}
