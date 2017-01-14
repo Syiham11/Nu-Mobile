d01fd8c025de9f902619469cff63369b6c0ab5f9
SitiSifa <siti.sitisifa@gmail.com>
2017-01-14 17:47:01 +0700

    add model lish data member user level

---
+++ app/src/main/java/com/adfin/numobile/model/CDataMemberUserLevel.java
@@ -0,0 +1,20 @@
+package com.adfin.numobile.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Siti on 1/14/2017.
+ */
+
+public class CDataMemberUserLevel {
+    private List<DataMemberUserLevel> DataMemberUserLevel = new ArrayList<DataMemberUserLevel>();
+
+    public List<DataMemberUserLevel> getDataMemberUserLevel() {
+        return DataMemberUserLevel;
+    }
+
+    public void setDataMemberUserLevel(List<DataMemberUserLevel> DataMemberUserLevel) {
+        this.DataMemberUserLevel = DataMemberUserLevel;
+    }
+}
